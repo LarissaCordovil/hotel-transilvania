c0870c95c4f8cfee1cfb7fa5f8bd8992172dc377
Christian Andrade <andrade.dias.christian@gmail.com>
2022-10-31 13:00:21 -0300

    create event button to save localStorage user infos

--- root/src/js/login.js
+++ root/src/js/login.js
@@ -1,4 +1,11 @@
 const fields = [ document.getElementById("name"), document.getElementById("email"), document.getElementById("password") ];
+// const form = document.getElementsByTagName("form")[0]
+const button = document.getElementsByTagName("button")[0]
+let valueField = {
+    name: "",
+    email: "",
+    password: ""
+}
 
 const emptyField = (value, label) => {
     if (value === "") {
@@ -14,7 +21,30 @@ const emptyField = (value, label) => {
 const handleField = (event) => {
     const value = event.target.value;
     const label = event.target.labels[0]
+    switch (event.target.name) {
+        case "name": 
+        valueField.name = value
+        break
+        case "email": 
+        valueField.email = value
+        break
+        case "password": 
+        valueField.password = value
+        break
+    }
     emptyField(value, label)
+    return
+}
+
+const handleButton = (event) => {
+    event.preventDefault()
+    if (valueField.name != "" && valueField.email != "" && valueField.password != "") {
+        for (const [key, value] of Object.entries(valueField)) {
+            localStorage.setItem(key.toString(), value)
+          }
+        return
+    }
+    alert("Preencha todos os campos!")
     return
 }
 
@@ -22,3 +52,6 @@ fields.map(field => {
     field.addEventListener("blur", handleField)
     field.addEventListener("change", handleField)
 })
+
+button.addEventListener("click", handleButton)
+
---
+++ root/src/js/users.js
@@ -0,0 +1,10 @@
+export const users = [
+    {
+        email: "teste@teste.com",
+        senha: "123"
+    },
+    {
+        email: "joao@teste.com",
+        senha: "12345"
+    }
+]

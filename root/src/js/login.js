ba4aa229c9d4d19db350c7629abc5a43e6b4576c
Christian Andrade <andrade.dias.christian@gmail.com>
2022-10-31 14:06:33 -0300

    adjust login with user and redirect

--- root/src/js/login.js
+++ root/src/js/login.js
@@ -1,4 +1,4 @@
-const fields = [ document.getElementById("name"), document.getElementById("email"), document.getElementById("password") ];
+const fields = [document.getElementById("name"), document.getElementById("email"), document.getElementById("password")];
 // const form = document.getElementsByTagName("form")[0]
 const button = document.getElementsByTagName("button")[0]
 let valueField = {
@@ -6,6 +6,16 @@ let valueField = {
     email: "",
     password: ""
 }
+
+export const users = [{
+        email: "teste@teste.com",
+        senha: "123"
+    },
+    {
+        email: "joao@teste.com",
+        senha: "12345"
+    }
+]
 
 const emptyField = (value, label) => {
     if (value === "") {
@@ -22,15 +32,15 @@ const handleField = (event) => {
     const value = event.target.value;
     const label = event.target.labels[0]
     switch (event.target.name) {
-        case "name": 
-        valueField.name = value
-        break
-        case "email": 
-        valueField.email = value
-        break
-        case "password": 
-        valueField.password = value
-        break
+        case "name":
+            valueField.name = value
+            break
+        case "email":
+            valueField.email = value
+            break
+        case "password":
+            valueField.password = value
+            break
     }
     emptyField(value, label)
     return
@@ -39,9 +49,18 @@ const handleField = (event) => {
 const handleButton = (event) => {
     event.preventDefault()
     if (valueField.name != "" && valueField.email != "" && valueField.password != "") {
-        for (const [key, value] of Object.entries(valueField)) {
-            localStorage.setItem(key.toString(), value)
-          }
+        let result = false
+         users.forEach(user => {if (user.email === valueField.email && user.senha === valueField.password) {
+            result = true
+         }})
+        if (result) {
+            for (const [key, value] of Object.entries(valueField)) {
+                localStorage.setItem(key.toString(), value)
+            }
+            window.location = "src/pages/home.html"
+            return
+        }
+        alert("Usuário não cadastrado!")
         return
     }
     alert("Preencha todos os campos!")
@@ -54,4 +73,3 @@ fields.map(field => {
 })
 
 button.addEventListener("click", handleButton)
-

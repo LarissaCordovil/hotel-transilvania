f3e5680da71a1825b62dfcd3ae0278109b47bb4e
Christian Andrade <andrade.dias.christian@gmail.com>
2022-11-11 00:22:53 -0300

    add modal more services

---
+++ root/src/js/reserva.js
@@ -0,0 +1,8 @@
+const button = document.getElementsByTagName('button')
+
+const handleButton = (event) => {
+    event.preventDefault()
+}
+
+button[0].addEventListener('click', handleButton)
+button[1].addEventListener('click', handleButton)

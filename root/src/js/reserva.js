2170d5794221902b6654ef06f462fe9f2a29349d
Andrade Christian <andrade.dias.christian@gmail.com>
2022-11-17 21:32:55 -0300

    create more services full application in modal

--- root/src/js/reserva.js
+++ root/src/js/reserva.js
@@ -1,8 +1,37 @@
-const button = document.getElementsByTagName('button')
+const listModal = document.querySelector('#list-modal');
+const labels = Array.from(listModal.querySelectorAll('label'));
+const btnConfirmServices = document.querySelector('#confirm-services');
+const textServices = document.querySelector('#services-text');
 
-const handleButton = (event) => {
-    event.preventDefault()
-}
+const services = ['mordomo', 'cofre', 'pet', 'cafe', 'massagem', 'ac'];
+let servicesObj = {};
+let totalServices = 0;
 
-button[0].addEventListener('click', handleButton)
-button[1].addEventListener('click', handleButton)
+const formatRealString = string => parseFloat(string.replace(',', '.'));
+
+const handleLabelModal = (element) => {
+    const input = element.target;
+    const price = formatRealString(input.labels[0].nextSibling.children[0].innerText);
+    (input.checked) ? localStorage.setItem(`service-${input.name}`, price.toString()) : localStorage.removeItem(`service-${input.name}`);
+};
+
+const handleModal = event => {
+    event.preventDefault();
+    servicesObj = {};
+    totalServices = 0;
+    services.map(element => {
+        const keyObj = element;
+        if (localStorage.getItem(`service-${element}`) !== null && localStorage.getItem(`service-${element}`) !== NaN) {
+            servicesObj[`${keyObj}`] = localStorage.getItem(`service-${element}`);
+            totalServices += parseFloat(localStorage.getItem(`service-${element}`));
+        }
+    });
+    textServices.innerHTML = `R$: ${totalServices}`;
+    //console.log para ajudar o Flaviano XD
+    console.log(servicesObj);
+    console.log(totalServices);
+};
+
+labels.map(label => label.onclick = label => handleLabelModal(label));
+
+btnConfirmServices.addEventListener('click', handleModal);;
